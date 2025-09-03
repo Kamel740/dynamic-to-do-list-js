a752d37a5c15e4b5343c6e08797cf1478d5897e1
kamel740 <kamelelsayed740@gmail.com>
2025-09-03 18:01:05 +0300

    oh yea

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 document.addEventListener('DOMContentLoaded',function(){
-
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
+
+    function saveTasks(){
+        const taskItmes = document.querySelectorAll('#task-list li');
+        var tasksArray = [];
+
+        taskItmes.forEach(element=>{
+           tasksArray.push(element.firstChild.textContent.trim());
+        });
+        localStorage.setItem('tasks',JSON.stringify(tasksArray));
+    }
+
+    const taskString = localStorage.getItem('tasks');
+    if(taskString){
+        const taskArray = JSON.parse(taskString);
+
+        taskArray.forEach(Item => {
+            const li_Item = document.createElement('li');
+            li_Item.textContent = Item;
+
+            const btn = document.createElement('button');
+            btn.textContent = 'Remove';
+            btn.classList.add('remove-btn');
+
+            li_Item.appendChild(btn);
+            taskList.appendChild(li_Item);
+        });
+
+    }
+
 
     function addTask(){
         const taskText = taskInput.value.trim();
@@ -23,6 +51,7 @@ document.addEventListener('DOMContentLoaded',function(){
             taskElement.appendChild(remove_button);
             taskList.appendChild(taskElement);
             taskInput.value = '';
+            saveTasks();
         }
 
     }
